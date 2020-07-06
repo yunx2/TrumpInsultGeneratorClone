51b8a4e9a41566764b6d168973f6203f315be077
Yunyun <celeph4is@gmail.com>
2020-07-06 15:48:43 -0700

    format entered text

--- insult-generator/src/App.js
+++ insult-generator/src/App.js
@@ -1,27 +1,74 @@
+/* eslint-disable default-case */
 import React, { useState } from 'react';
 import insults from './trump'
 
 function App() {
   const [name, setName] = useState('')
   const [insult, setInsult] = useState('')
+  const [insultType, setInsultType] = useState(null)
 
   const handleNameChange = e => {
     e.preventDefault()
-    setName(e.target.value);
+    setName(e.target.value)
   }
 
-  const getRandomIndex = (component) => {
+  const getRandomIndex = component => {
     return Math.floor(Math.random() * Math.floor(component.length))
   }
 
   const generateInsult = () => {
+    const randomDescriptorAfter = insults.descriptor_after[getRandomIndex(insults.descriptor_after)]
+    const randomDescriptorBefore = insults.descriptor_before[getRandomIndex(insults.descriptor_before)]
+    const randomPredicate = insults.predicate[getRandomIndex(insults.predicate)]
+    const randomInsult = insults.insult[getRandomIndex(insults.insult)]
+    const randomKicker = insults.kicker[getRandomIndex(insults.kicker)]
+    const randomSubjectInMiddle = insults.subject_in_middle[getRandomIndex(insults.subject_in_middle)]
+
     if (name) {
-      const randomDescriptorAfter = insults.descriptor_after[getRandomIndex(insults.descriptor_after)]
-      const randomPredicate = insults.predicate[getRandomIndex(insults.predicate)]
-      const randomInsult = insults.insult[getRandomIndex(insults.insult)]
-      const randomKicker = insults.kicker[getRandomIndex(insults.kicker)]
+      console.log(name)
+      const formattedName = name[0].toUpperCase() + name.slice(1).toLowerCase()
+      console.log(formattedName)
+
+      setInsultType(Math.floor(Math.random() * Math.floor(4)))
+      console.log(insultType)
+      let generatedInsult;
+      switch (insultType) {
+        case 0:
+          generatedInsult =  formattedName + randomDescriptorAfter + randomPredicate + randomInsult
+          if (Math.random() > .5) {
+              generatedInsult += randomKicker
+            }
+          break
+        case 1:
+          generatedInsult = randomDescriptorBefore + formattedName + randomPredicate + randomInsult
+          if (Math.random() > .5) {
+            generatedInsult += randomKicker
+          }
+          break
+        case 2:
+          generatedInsult = formattedName + randomPredicate + randomInsult
+          if (Math.random() > .5) {
+            generatedInsult += randomKicker
+          }
+          break
+        case 3:
+          generatedInsult = randomDescriptorBefore + formattedName + randomDescriptorAfter + randomPredicate + randomInsult
+          if (Math.random() > .5) {
+            generatedInsult += randomKicker
+          }
+          break
+        case 4: 
+          generatedInsult = randomSubjectInMiddle[0] + formattedName + randomSubjectInMiddle[1] + formattedName + randomPredicate + randomInsult
+          if (Math.random() > .5) {
+            generatedInsult += randomKicker
+          }
+          break
+      }
       
-      setInsult(name + ' ' + randomDescriptorAfter + ' ' + randomPredicate + ' ' + randomInsult + ' ' + randomKicker)
+      setInsult(generatedInsult)
+    } else {
+      console.log('no name given')
+      setInsult('')
     }
   }
 
@@ -31,7 +78,7 @@ function App() {
     name: 
       <input type="text" onChange={handleNameChange} />
       <button type="button" onClick={generateInsult}>generate insult</button>
-    {insult}
+    <div>{insult}</div>
     </div>)
   
 }
