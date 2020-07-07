f8c40ff7fe5a146d009d9f90e863df79ee6dc979
Yunyun <celeph4is@gmail.com>
2020-07-07 10:18:26 -0700

    add bootstrap and reactstrap

--- insult-generator/src/index.js
+++ insult-generator/src/index.js
@@ -1,6 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
 ReactDOM.render(
@@ -8,7 +8,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 
