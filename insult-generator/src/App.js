e7584c7d7cf6c4fd7318cc275b6b52381200e259
Yunyun <celeph4is@gmail.com>
2020-07-08 21:53:08 -0700

    add styling using bootstrap

--- insult-generator/src/App.js
+++ insult-generator/src/App.js
@@ -1,6 +1,21 @@
 /* eslint-disable default-case */
 import React, { useState } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Nav from 'react-bootstrap/Nav'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
+import InputGroup from 'react-bootstrap/InputGroup'
+
+
+
 import axios from 'axios'
+import { FormLabel } from 'react-bootstrap';
 
 function App() {
   const [name, setName] = useState('')
@@ -24,13 +39,29 @@ function App() {
 
   return (
     <div className="App">
-    Trump Insult Generator <br />
-    name: 
-      <input type="text" onChange={handleNameChange} />
-      <button type="button" onClick={handleClick}>generate insult</button>
-      <div>{insult}</div>
+      <Container fluid>
+        <h1 className="display-4">
+          <p className="text-center">Trump Insult Generator</p>
+        </h1>
+        <Row>
+          <Col>
+            <Image src="https://trumpinsultgenerator.com/images/Trump.jpg" fluid className="float-left"/>
+          </Col>
+          <Col>
+            <Form.Group>
+              <span>
+              <Form.Label>Name: </Form.Label>
+              <Form.Control type="text" onChange={handleNameChange} placeholder="Enter name" />
+              </span>
+              
+            </Form.Group>
+            <Button variant="primary" onClick={handleClick}>Generate Insult</Button>
+            <Form.Control as="textarea" rows="5" className="mt-3"/>
+
+          </Col>
+        </Row>
+      </Container>
     </div>)
-  
 }
 
 export default App;
